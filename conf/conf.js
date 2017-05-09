fabf674dd7ce26de2d5ac7f8a15486eca8c443f3
lovejoy <personalovejoy@gmail.com>
2017-05-09 23:06:54 +0800

    Add Front End

--- conf/conf.js
+++ conf/conf.js
@@ -2,7 +2,7 @@ module.exports = {
     db: {
         host: 'localhost',
         user: 'root',
-        password: '5898zHy1627',
-        database: 'Cooperator'
+        password: '',
+        database: 'cooperator'
     }
 };
