ec50629a5ab05437cda64ec22be2af8158c694ee
MHBZHY <mianhuabao@126.com>
2017-05-09 23:56:27 +0800

    添加chatroom；基本添加模版对应的render项（仍需数据填充）；无需render的文件放回public目录

--- routes/static.js
+++ routes/static.js
@@ -3,22 +3,76 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.redirect('/index.html');
+    res.redirect('/MyCollerApp2/index.html');
+});
+router.get('/', function (req, res) {
+
 });
 
 // MyCollerApp2
-var router1 = express.Router();
+var routerStatic = express.Router();
+var routerChat = express.Router();
 
-router.use('/MyCollerApp2', router1);
+router.use('/MyCollerApp2', routerStatic);
+router.use("/ChatRoom", routerChat);
 
-router1.get('/myinformation.html', function (req, res) {
-	res.render('myinformation.html', {
+routerStatic.get('/myinformation.html', function (req, res) {
+	res.render('myinformation', {
 		myStates: 'Coming Soon',    //我的状态
 		visitNum: 'Coming Soon',    //访问数
-		perTag: [   //需求tag
+		perTag: [                   //需求tag
 			'123', '456', '789'
 		],
-		newReqIntro: '' //需求介绍
+		newReqIntro: ''             //需求介绍
+	})
+});
+
+routerStatic.get('/tab-webview-subpage-about.html', function (req, res) {
+	res.render('tab-webview-subpage-about', {
+		proTitle: '',   //需求标题 ex:图片处理以及卷积神经网络研究
+		PerIntro: '',   //个人介绍
+		reqtag: [
+			'123', '456', '789'
+		],              //需求标签
+		prodesc: '',    //需求描述
+		proReq: ''      //项目要求
+	})
+});
+
+routerStatic.get('/tab-webview-subpage-chat.html', function (req, res) {
+	res.render('tab-webview-subpage-chat', {
+		FriendName: '',     //朋友名称
+		FriendPosition: '', //朋友地位／职位
+		FirendTel: ''       //朋友联系方式
+	})
+});
+
+routerStatic.get('/tab-webview-subpage-contact.html', function (req, res) {
+	res.render('tab-webview-subpage-contact', {
+		myStates: 'Coming Soon',    //我的状态
+		visitNum: 'Coming Soon',    //访问数
+		perTag: [                   //需求tag
+			'123', '456', '789'
+		]
+	})
+});
+
+routerStatic.get('/tab-webview-subpage-req_information.html', function (req, res) {
+	res.render('tab-webview-subpage-req_information', {
+		proTitle: '',       //需求标题 ex:图片处理以及卷积神经网络研究
+		PerIntro: '',       //个人介绍
+		reqtag: [
+			'123', '456', '789'
+		],                  //需求标签
+		prodesc: '',        //需求描述
+		proReq: '',         //项目要求
+		relPerson: '',      //项目发起人的实名
+		PerPosition: '',    //职位
+		skill: '',          //技能
+		perTag: [           //需求tag
+			'123', '456', '789'
+		],
+		fullReqDesc: ''     //需求的详细描述信息
 	})
 });
 
